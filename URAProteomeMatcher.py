1a820b69a406e4efa06a025920b63851574ab130
RileyRis <rileyristeen@gmail.com>
2025-03-31 12:08:57 -0400

    incorperated the predicted activation state of each upstream regulator into the output, ensuring only significant URs are included

--- URAProteomeMatcher.py
+++ URAProteomeMatcher.py
@@ -12,21 +12,21 @@ from classes.Config import Config
 from classes.UI import *
 
 def arg_parser():
-    """ Defines named arguments, provides the --help/-h flag, and allows the option to use required arguments """
-    parser = argparse.ArgumentParser(description = 'arguments')
-    parser.add_argument('-pd', '--proteome-data-file', help = 'REQUIRED: Local file path to proteome data', nargs = '?', type = str, required = True)
-    parser.add_argument('-ur', '--upstream-regulator-file', help = 'REQUIRED: Local file path to upstream regulator data', nargs = '?', type = str, required = True)
-    parser.add_argument('-ug', '--upstream-regulator-group-file', help = 'OPTIONAL: Local file path to json gene group file', nargs = '?', type = str, required = False)
-    parser.add_argument('-ipn', '--ignore-protein-name-matches', help = 'OPTIONAL: Only match Genes, ignore Protein_Names matches', action = 'store_true')
-    parser.add_argument('-ll', '--log-level', help = 'OPTIONAL: Log level, defaults to INFO', nargs = '?', type = str, required = False, default='INFO')
-    parser.add_argument('-lf', '--log-to-file', help = 'OPTIONAL: Log to file, defaults to False', action = 'store_true')
-    parser.add_argument('-of', '--out-file', help = 'OPTIONAL: Local file to output results to', nargs = '?', type = str, required = False)
-    return parser.parse_args()
+      """ Defines named arguments, provides the --help/-h flag, and allows the option to use required arguments """
+      parser = argparse.ArgumentParser(description = 'arguments')
+      parser.add_argument('-pd', '--proteome-data-file', help = 'REQUIRED: Local file path to proteome data', nargs = '?', type = str, required = True)
+      parser.add_argument('-ur', '--upstream-regulator-file', help = 'REQUIRED: Local file path to upstream regulator data', nargs = '?', type = str, required = True)
+      parser.add_argument('-ug', '--upstream-regulator-group-file', help = 'OPTIONAL: Local file path to json gene group file', nargs = '?', type = str, required = False)
+      parser.add_argument('-ipn', '--ignore-protein-name-matches', help = 'OPTIONAL: Only match Genes, ignore Protein_Names matches', action = 'store_true')
+      parser.add_argument('-ll', '--log-level', help = 'OPTIONAL: Log level, defaults to INFO', nargs = '?', type = str, required = False, default='INFO')
+      parser.add_argument('-lf', '--log-to-file', help = 'OPTIONAL: Log to file, defaults to False', action = 'store_true')
+      parser.add_argument('-of', '--out-file', help = 'OPTIONAL: Local file to output results to', nargs = '?', type = str, required = False)
+      return parser.parse_args()
 
 def main(args):
     """ Main function, parses proteome data file and adds upstream regulator matches/groups """
-    # instantiate the logger and matcher
-    current_time = datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
+    # # instantiate the logger and matcher
+    current_time = datetime.now().isoformat()
     logger = Logger.get_logger(args.log_level, args.log_to_file, current_time)
     matcher = URAProteomeMatcher(logger)
 
@@ -49,28 +49,44 @@ def main(args):
         expected_columns = ['Protein_Names', 'Genes']
     matcher.validate_proteome_data(proteome_data, expected_columns)
 
-    # find values for exact, group, and best match columns
+    # find URs for exact, group, and best match columns
     print('\n--- look for matches, generate match data ---\n')
     match_data = []
-    # error handle this in case protein names isnt present
+    ## error handle this in case protein names isnt present
     for index, row in proteome_data.iterrows():
         genes = row.get('Genes', '').split(';')
         protein_names = row.get('Protein_Names', '').split(';')
 
         ur_exact_matches = matcher.get_ur_exact_matches(index, upstream_regulators, genes, protein_names, args.ignore_protein_name_matches)
-        ur_group_matches = matcher.get_ur_group_matches(index, group_data, genes)
+        ur_group_matches = matcher.get_ur_group_matches(index,upstream_regulators, group_data, genes)
         ur_best_matches = matcher.get_ur_best_matches(index, ur_exact_matches, ur_group_matches)
 
         match_data.append([';'.join(ur_exact_matches), ';'.join(ur_group_matches), ';'.join(ur_best_matches)])
 
     # write match data to the proteome data
     print('\n--- update proteome data with matches---\n')
-    updated_proteome_data = matcher.update_proteome_data(proteome_data, match_columns, match_data)
+    updated_proteome_data = matcher.update_proteome_data(proteome_data, match_columns, match_data)    
+    
 
+    # find the predicted activation state for your UR EXACT_MATCH and BEST_MATCH columns
+    prediction_data = []
+    for index, row in updated_proteome_data.iterrows():
+        exact_match_urs = row.get('UR_EXACT_MATCH', '').split(';')
+        ur_em_predictions = matcher.get_em_predictions(index, upstream_regulators, exact_match_urs)
+        
+        best_match_urs = row.get('UR_BEST_MATCH', '').split(';')
+        ur_bm_predictions = matcher.get_bm_predictions(index, upstream_regulators, best_match_urs)
+        
+        prediction_data.append([';'.join(ur_em_predictions), ';'.join(ur_bm_predictions)])
+    
+    predic_columns = ['EXACT_MATCH_PREDIC', 'BEST_MATCH_PREDIC']
+    proteome_data_withpredicts = matcher.update_proteome_data(updated_proteome_data, predic_columns, prediction_data)  
+
+        
     # write the updated proteome data to a file
     print('\n--- write updated proteome data to file ---\n')
     out_file = os.path.abspath(args.out_file or f'URAProteomeMatcher_OUT_{current_time}.csv')
-    matcher.write_dataframe_file(updated_proteome_data, out_file)
+    matcher.write_dataframe_file(proteome_data_withpredicts, out_file) 
     return out_file
 
 def ui_main():
@@ -80,7 +96,7 @@ def ui_main():
     frame.pack(fill=X)
 
     pd_entry = create_entry(frame, '')
-    pd_button = create_button(frame, 'Select Proteome File', get_file_path, pd_entry)
+    pd_button = create_button(frame, 'Select Proteome CSV File', get_file_path, pd_entry)
 
     ur_entry = create_entry(frame, '')
     ur_button = create_button(frame, 'Select Upstream Regulator List', get_file_path, ur_entry)
@@ -131,4 +147,5 @@ if __name__ == "__main__":
             main(args)
     except Exception as e:
         # wrap main in a try/except to raise an Exception as soon as an error occurs
-        raise Exception('Error', e)
+        raise Exception('Error', e) 
+
--- classes/Matcher.py
+++ classes/Matcher.py
@@ -4,9 +4,10 @@ import os
 import json
 import pandas as pd
 import logging
-#import collections
 
 from tabulate import tabulate
+import re
+
 
 class URAProteomeMatcher():
     """ Class used to identify upstream regulators for given protein or gene names """
@@ -58,7 +59,7 @@ class URAProteomeMatcher():
         Creates a list of upstream regulators to search from from a given file (containing URs seperated by newline), e.g.
 
         ITAE
-        IL-17 receptor family
+        IL-17R family
         CD5
         THRB
         TLR4 complex (LPS-binding)
@@ -66,20 +67,43 @@ class URAProteomeMatcher():
         DRD5
         FOS
         TLR4
-        AP-2 complex
 
 
         """
         file_path = os.path.abspath(upstream_regulator_file)
+        
+        #when loading the tsv, replace any empty values with a " " (need this to avoid issues later)
+        ur_data = pd.read_csv(file_path, sep="\t").replace({float('nan'): ' '})
 
-        # # do we want to keep this as a csv file? seems like it doesn't have to be
-        upstream_regulators = []
-        with open(file_path) as file:
-            upstream_regulators = list(set(filter(None, file.read().split("\n"))))
-            self.logger.info(f'LOADED UPSTREAM REGULATORS: {file_path}')
-            self.logger.debug('\n' + tabulate({ None : upstream_regulators }, ['UPSTREAM_REGULATORS'], tablefmt='fancy_grid') + '\n')
-            file.close()
+        self.logger.info(f'LOADED UPSTREAM REGULATORS: {file_path}')
+        
+        #identify the columns containing ur names and predicted activation status
+        ur_data.columns = map(str.lower, ur_data.columns)
+        if len(ur_data.filter(regex='upstream.regulator').columns) == 1:
+            ur_colname = ur_data.filter(regex='upstream.regulator').columns[0]
+        else:
+            Exception("Upstream regulator column not identified. Please ensure there is exactly one column named 'upstream_regulator'")
 
+        if len(ur_data.filter(regex='predict.*').columns) == 1:
+            predic_colname = ur_data.filter(regex='predict.*').columns[0]
+        else:
+            Exception("predicted activation column not identified. Please ensure there is exactly one column named 'prediction'")
+         
+        #empty values in the predicted activation column mean the UR is not significant or not predicted to be act/inhib. Change to "n.s."
+        ur_data.loc[ur_data[predic_colname] == ' ', predic_colname] = 'n.s.'
+
+        #create a dictionary with the urs as keys and the predicitons as values
+        upstream_regulators = ur_data[[ur_colname,predic_colname]].set_index(ur_colname).to_dict()[predic_colname]
+        self.logger.debug('\n' + tabulate({ None : upstream_regulators }, ['UPSTREAM_REGULATORS'], tablefmt='fancy_grid') + '\n')
+                
+        #Remove URs that are not significantly activated or inhibited (unless data contains no Activated/Inhibited URs)
+        not_significant = [ur for ur,pred in upstream_regulators.items() if pred == 'n.s.']
+        if len(not_significant) < len(upstream_regulators):
+            for ur in not_significant: del upstream_regulators[ur]
+            self.logger.info('THE FOLLOWING UPSTREAM REGULATORS WERE REMOVED (NOT ACTIVATED OR INHIBITED)' +  '\n - ' + '\n - '.join(not_significant))
+        else:
+            self.logger.error('NO UPSTREAM REGULATORS RECOGNIZED AS SIGNIFICANT, SO ALL HAVE BEEN INCLUDED')
+        
         return upstream_regulators
 
     def load_group_data(self, group_file: str):
@@ -123,7 +147,7 @@ class URAProteomeMatcher():
                 dataframe.drop(column, axis=1)
         self.logger.info('DONE')
 
-    def get_ur_exact_matches(self, index: int, upstream_regulators: list, genes: list, protein_names: list, ignore_protein_name_matches: bool):
+    def get_ur_exact_matches(self, index: int, upstream_regulators: dict, genes: list, protein_names: list, ignore_protein_name_matches: bool):
         """
         Looks for upstream regulator matches in a list of genes, and optionally in protein names
         For gene matches, the upstream regulator must be an exact match, e.g.
@@ -137,7 +161,7 @@ class URAProteomeMatcher():
         Matches will get written in the UR_EXACT_MATCH column as a ; separated list
         """
         ur_exact_matches = set()
-        for regulator in upstream_regulators:
+        for regulator, predic in upstream_regulators.items():
             if regulator in genes:
                 self.logger.info(f'ROW {index} UR_EXACT_MATCH {regulator} FOUND IN Genes: {";".join(genes)}')
                 ur_exact_matches.add(regulator)
@@ -149,7 +173,7 @@ class URAProteomeMatcher():
                 pass
         return ur_exact_matches
 
-    def get_ur_group_matches(self, index: int, group_data: dict, genes: list):
+    def get_ur_group_matches(self, index: int, upstream_regulators: dict, group_data: dict, genes: list):
         """
         Looks for group matches based on a provided JSON file containing groups and the gene names within them, e.g.
         {
@@ -161,18 +185,19 @@ class URAProteomeMatcher():
         For each gene in a row, this will search for a match in the dict values
         If a match is found, the group key followed by the matched gene will get recorded in the UR_GROUP_MATCH column, e.g.
 
-            IL-17 receptor family [IL17RB]
+            IL-17R family [IL17RB]
         
         """
         group_matches = {}
         if group_data:
             for group, group_genes in group_data.items():
-                for gene in genes:
-                    if gene in group_genes:
-                        if group not in group_matches.keys():
-                            group_matches[group] = []
-                        self.logger.info(f'ROW {index} UR_GROUP_MATCH {group} [{gene}] FOUND')
-                        group_matches[group].append(gene)
+                if group in upstream_regulators:
+                    for gene in genes:
+                        if gene in group_genes:
+                            if group not in group_matches.keys():
+                                group_matches[group] = []
+                            self.logger.info(f'ROW {index} UR_GROUP_MATCH {group} [{gene}] FOUND')
+                            group_matches[group].append(gene)
 
         ur_group_matches = set([f"{k} [{','.join(v)}]" for k, v in group_matches.items()])
         return ur_group_matches
@@ -203,6 +228,24 @@ class URAProteomeMatcher():
         self.logger.info('UPDATED PROTEOME WITH URS')
         self.logger.debug('\n' + tabulate(updated_proteome_data, updated_proteome_data.columns, tablefmt='fancy_grid') + '\n')
         return updated_proteome_data
+    
+    
+    def get_em_predictions(self, index: int, upstream_regulators: dict, exact_match_urs: list):
+       predictions = []
+       for regulator, predic in upstream_regulators.items():
+           if regulator in exact_match_urs:
+               predictions.append(predic)
+               self.logger.info(f'ROW {index} UR_EXACT_MATCH {regulator} PREDICTION: {predic}')
+       return predictions
+
+    def get_bm_predictions(self, index: int, upstream_regulators: dict, best_match_urs: list):
+        predictions = []
+        for regulator, predic in upstream_regulators.items():
+            if regulator in [re.sub(r' \[.*\]', '', i) for i in best_match_urs]:
+                predictions.append(predic)
+                self.logger.info(f'ROW {index} UR_BEST_MATCH {regulator} PREDICTION: {predic}')
+        return predictions  
+    
 
     def write_dataframe_file(self, dataframe: pd.DataFrame, out_file: str):
         OUT_FILE = os.path.abspath(out_file)
